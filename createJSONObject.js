7920bdd5f399f155e4201822b863c889638a0741
aleclivinghouse <aleclivinghouse@yahoo.com>
2018-07-31 11:50:06 -0700

    asdf

--- createJSONObject.js
+++ createJSONObject.js
@@ -8,9 +8,9 @@ function Side(){
   this.molecules = [];
 };
 
-function Equation(side1, side2){
-  this.side1 = side1;
-  this.side2 = side2;
+function Equation(reactants, products){
+  this.reactants = reactants;
+  this.products = products;
 };
 
 Side.prototype.addMolecule = function(object){
@@ -33,11 +33,13 @@ Molecule.prototype.contains = function(name){
   }
   return false;
 }
-//////////////////////////
-///////////////////////////
-let data = "1C6H12O6+6O2=6Co2+6H2O";
+
+////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+ // let data = "1C6H12O6+6O2=6Co2+6H2O";
+let data = '2FeCl3+MgO=Fe2O3+MgCl2'
 let sides = data.split("=");
-console.log(sides);
 let leftMolecules = sides[0].split("+");
 let rightMolecules = sides[1].split("+");
 
@@ -50,15 +52,18 @@ function sideToJSON(side){
     let lowerCaseRegex = /[a-z]/;
     let numRegex = /[0-9]/;
     let co = parseInt(molecule[0]);
+    if(numRegex.test(co)===false){
+      co = 1;
+    }
     let theMolecule = new Molecule(co);
 
 
     let rest = molecule.substr(1);
 
-    for(let j = 0; j<rest.length; j++){
-
+    for(let j = 0; j<rest.length-1; j++){
       //if the char is a capital letter
       if(capitalRegex.test(rest[j]) === true){
+
         //Capital LowerCase Number Number
         if(lowerCaseRegex.test(rest[j+1]) === true && numRegex.test(rest[j+2]) === true && numRegex.test(rest[j+3]) === true){
           let atom = '';
@@ -119,4 +124,59 @@ function sideToJSON(side){
  let reactants = sideToJSON(leftMolecules);
  let products = sideToJSON(rightMolecules);
  let equation = new Equation(reactants, products);
- console.log(equation);
+
+ ///////////////////////////////////
+ ///////////////////////////////////
+
+ function balance(equation){
+   let reactants = buildMap(equation.reactants);
+   let products = buildMap(equation.products);
+
+   //if the maps are not equal on both sides
+   for(let item in reactants){
+     if(reactants[item] < products[item]){
+       let theAtom = item;
+       let reactants = equation.reactants;
+       for(let molecule of reactants.molecules){
+         if(molecule.contains(theAtom)){
+             molecule.coefficent+=1;
+           }
+           console.log(equation);
+           return balance(equation);
+         }
+       } //if end
+
+    if(reactants[item] > products[item]){
+          let theAtom = item;
+          let products = equation.products;
+          for(let molecule of products.molecules){
+            if(molecule.contains(theAtom)){
+                molecule.coefficent+=1;
+                return balance(equation);
+              }
+            }
+          } //if end
+      }
+       return equation;
+    } //balance end
+//////////////////////////////
+/////////////////////////////
+
+
+function buildMap(side){
+    let map = {};
+    for(let molecule of side.molecules){
+        let coefficent = molecule.coefficent;
+        for(let atom of molecule.atoms){
+          if(!map[atom.name]){
+           map[atom.name] = atom.subscript * coefficent;
+         } else {
+           map[atom.name] += atom.subscript * coefficent;
+         }
+        }
+    }
+     return map;
+} //buildMap end
+
+
+balance(equation);
