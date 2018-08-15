9025b4483e46a3cd2d218eb9dc5a460947f1b795
aleclivinghouse <aleclivinghouse@yahoo.com>
2018-08-15 11:00:10 -0700

    works but gives fractions

--- createJSONObject.js
+++ createJSONObject.js
@@ -50,9 +50,9 @@ aCoefficent.prototype.solve = function(value){
 }
 ////////////////////////////////////////////////////
 /////////////////////////////////////////////////////
-
- // let data = "1C6H12O6+6O2=6Co2+6H2O";
-let thingy = 'B5H9+O2=B2O3+H2O';
+  //
+ let thingy = 'B5H9+O2=B2O3+H2O';
+  // let thingy = 'Li+H3PO4=H2+Li3PO4';
 
 
 function stringToJSON(data){
@@ -121,11 +121,9 @@ function sideToJSON(side){
            theMolecule.addAtom(atom, parseInt(subscript));
 
         //Just a Capital on Its Own
-      } else if(numRegex.test(rest[j])===true){
+      } else {
            let anAtom = rest[j];
            theMolecule.addAtom(anAtom, 1);
-         } else {
-           throw Error;
          }
       }
     }
@@ -211,7 +209,6 @@ let putItIn = createEquations(next);
 function balance(map){
  let coefficentObjects = createCoefficentObjects(map);
  let values = Object.values(map);
-  console.log('these are the equations to begin with ' + values);
  let firstLetter = findFirstLetter(map);
  let equationsToSolve = setLetterToOne(values, firstLetter);
  let newCoefficentObjects = setCoefficentObjectOne(coefficentObjects, firstLetter);
@@ -232,8 +229,34 @@ function balanceRest(theCoefficentObjects, theEquationsToSolve, i){
    return balanceRest(coefficentObjects2, equationsToSolve2, i+1);
 } //balance rest end
 
-let print = balance(putItIn);
- console.log(print);
+let mapLCM = balance(putItIn);
+let arrLCM = lcmInput(mapLCM);
+let print = numsLCM(arrLCM);
+console.log(print);
+
+
+function lcmInput(map){
+  let arr = [];
+  for(let thing of map){
+    console.log(thing);
+    arr.push(thing.value);
+  }
+  return arr
+}
+
+function numsLCM(arr){
+  let newArr = [];
+  for(let i =0; i < arr.length; i++){
+    arr[i] = arr[i].toString();
+    if(arr[i].indexOf('/')){
+       newArr.push(parseInt(arr[i].split('/')[1]));
+    }
+  }
+  let theArr = removeDuplicates(newArr);
+  return theArr;
+}
+
+
 //////////////////////////////
 ////////////////////////////
 //BUG solve is not solving correctly
@@ -249,153 +272,130 @@ function solve(solveNext){
       letter += solveNext[i];
     }
   }
-
   sides = solveNext.split('=');
+  ////////////////////////////////
+  ////////////////////////////////
   let leftSide = sides[0];
   let rightSide = sides[1];
-  let sideToUse = '';
-  let otherSide = '';
+  let sideWithVariable = '';
+  let justNumbers = '';
+  let flag = false;
   if(leftSide.includes(letter)){
-    sideToUse += leftSide;
-    otherSide += rightSide;
+    sideWithVariable += leftSide;
+    justNumbers = rightSide;
   } else {
-    sideToUse += rightSide;
-    otherSide += leftSide;
+    sideWithVariable += rightSide;
+    justNumbers = leftSide;
   }
 
-  let expression = new Expression(letter);
-  sideToUse = sideToUse.replace(" ", "");
-  for(let i = 0; i < sideToUse.length; i++){
-    //in the case of -32x
-    console.log('this is what were trying to solve ' + sideToUse);
-    if(sideToUse[i]==='-' && numRegex.test(sideToUse[i+1])===true && numRegex.test(sideToUse[i+2])===true && lowerCaseRegex.test(sideToUse[i+3])===true){
-      let strToCut = '';
-      let numStr ='';
-      let theStart = 1;
-      numStr+= sideToUse[i+1];
-      numStr+= sideToUse[i+2];
-      strToCut+=sideToUse[i];
-      strToCut+=sideToUse[i+1];
-      strToCut+=sideToUse[i+2];
-      strToCut+=sideToUse[i+3];
-      let rest = sideToUse.replace(strToCut, '');
-      while(theStart < parseInt(numStr)){
-        expression.subtract(letter);
-        theStart++;
-      } //while end
-      expression.add(rest);
-      console.log('this is the finished expression: ' + expression);
-      console.log('this string gets cut ' + strToCut);
-      console.log('this is numStr ' + numStr);
-    } //if end
-       //in the case of +32x
-    if(sideToUse[i]==='+' && numRegex.test(sideToUse[i+1])===true && numRegex.test(sideToUse[i+2])===true && lowerCaseRegex.test(sideToUse[i+3])===true){
-      let strToCut = '';
-      let numStr ='';
-      let theStart = 1;
-      numStr+= sideToUse[i+1];
-      numStr+= sideToUse[i+2];
-      strToCut+=sideToUse[i];
-      strToCut+=sideToUse[i+1];
-      strToCut+=sideToUse[i+2];
-      strToCut+=sideToUse[i+3];
-      let rest = sideToUse.replace(strToCut, '');
-      while(theStart < parseInt(numStr)){
-        expression.add(letter);
-        theStart++;
-      } //while end
-      expression.add(rest);
-      console.log('this is the finished expression: ' + expression);
-      console.log('this string gets cut ' + strToCut);
-      console.log('this is numStr ' + numStr);
-    } //if end
-        // in the case of -3x
-    if(sideToUse[i]==='-' && numRegex.test(sideToUse[i+1])===true && lowerCaseRegex.test(sideToUse[i+2])===true){
-          let strToCut = '';
-          let numStr ='';
-          let theStart = 1;
-          numStr+=sideToUse[i+1];
-          strToCut+=sideToUse[i];
-          strToCut+=sideToUse[i+1];
-          strToCut+=sideToUse[i+2];
-          let rest = sideToUse.replace(strToCut, '');
-          while(theStart < parseInt(numStr)){
-            expression.subtract(letter);
-            theStart++;
-          } //while end
-          expression.add(rest);
-          console.log('this is the finished expression: ' + expression);
-          console.log('this string gets cut ' + strToCut);
-          console.log('this is numStr ' + numStr);
-        }//if end
+  justNumbers = eval(justNumbers);
 
-        //in the case of +3x
-     if(sideToUse[i]==='+' && numRegex.test(sideToUse[i+1])===true && lowerCaseRegex.test(sideToUse[i+2])===true){
-              let strToCut = '';
-              let numStr ='';
-              let theStart = 1;
-              numStr+=sideToUse[i+1];
-              strToCut+=sideToUse[i];
-              strToCut+=sideToUse[i+1];
-              strToCut+=sideToUse[i+2];
-              let rest = sideToUse.replace(strToCut, '');
 
-              while(theStart < parseInt(numStr)){
-                expression.add(letter)
-                theStart++;
-              } //while end
-              expression.add(rest);
-              console.log('this is the finished expression:' + expression);
-              console.log('this string gets cut ' + strToCut);
-              console.log('this is numStr ' + numStr);
-            }//if end
+  if(lowerCaseRegex.test(sideWithVariable[0])===true && flag===false){
+    //b+5=20
+    let flag = true;
+    let letter = sideWithVariable[0];
+    let toSubtract = sideWithVariable.replace(letter, '');
+    //b = 20 -5
+    if(justNumbers[justNumbers.length-1]==='+'){
+      justNumbers = justNumbers.substring(0, justNumbers.length-1);
+    }
+    if(toSubtract.length > 0){
+    justNumbers += '-' + toSubtract;
+    }
+    let newRight = eval(justNumbers).toString();
+    let isolatedVar = letter + '=' + newRight;
+    var completed = isolatedVar;
+  }
 
-      //in the case of 3x
-      if(numRegex.test(sideToUse[i])===true && lowerCaseRegex.test(sideToUse[i+1])===true){
-            let strToCut = '';
-            let numStr ='';
-            let theStart = 1;
-            numStr+=sideToUse[i];
-            strToCut+=sideToUse[i];
-            strToCut+=sideToUse[i+1];
-            let rest = sideToUse.replace(strToCut, '');
-           while(theStart < parseInt(numStr)){
-              expression.add(letter);
-              theStart++;
-            } //while end
-           expression.add(rest);
-           console.log('this is the finished expression: ' + expression);
-           console.log('this string gets cut ' + strToCut);
-           console.log('this is numStr ' + numStr);
-          }//if end
+
+  for(let i = 0; i < sideWithVariable.length; i++){
+  let numVar = '';
+  //20b + 33 = 60
+  if(numRegex.test(sideWithVariable[i])===true && numRegex.test(sideWithVariable[i+1])===true && lowerCaseRegex.test(sideWithVariable[i+2])===true && flag===false){
+    flag = true;
+    numVar+=sideWithVariable[i];
+    numVar+=sideWithVariable[i+1];
+    numVar+=sideWithVariable[i+2];
+    let toSubtract = sideWithVariable.replace(numVar, '');
+    if(justNumbers[justNumbers.length-1]==='+'){
+      justNumbers = justNumbers.substring(0, justNumbers.length-1);
+    }
+    if(toSubtract.length > 0){
+    justNumbers += '-' + toSubtract;
+    }
+    let newRight = eval(justNumbers).toString();
+    let newEquation = numVar + '=' + newRight;
+    let theNum = numVar[0] + numVar[1];
+    let isolatedVar = numVar[2] + '=' + newRight + '/' + theNum;
+
+  }
+
+  //2b + 5 = 10
+  if(numRegex.test(sideWithVariable[i])===true && lowerCaseRegex.test(sideWithVariable[i+1])===true && flag===false){
+    flag = true;
+    numVar+=sideWithVariable[i];
+    numVar+=sideWithVariable[i+1];
+    //numVar: 2b
+    let toSubtract = sideWithVariable.replace(numVar, '');
+    //toSubtract: +5
+    //BUG this line is not working the way it should
+    if(toSubtract.length > 0){
+    justNumbers += '-' + toSubtract;
+    }
+    //justNumbers: 10 - + 5
+    if(justNumbers[justNumbers.length-1]==='+'){
+      justNumbers = justNumbers.substring(0, justNumbers.length-1);
+    }
+    console.log('this is justNumbers ' + justNumbers);
+    let newRight = eval(justNumbers).toString();
+    let newEquation = numVar + '=' + newRight;
+    let isolatedVar = numVar[1] + '=' + newRight + '/' + numVar[0];
+    var completed = isolatedVar;
+  } //first if end
+
+  //the if statement isnt quite right
+  if(numRegex.test(sideWithVariable[i])===false && lowerCaseRegex.test(sideWithVariable[i+1])===true && flag===false){
+    //x + 5 = 10
+    let letter = sideWithVariable[i+1];
+    let nums = sideWithVariable.replace(letter, '');
+    let newRight = justNumbers + '-' + nums;
+    let final = parseInt(newRight);
+    let isolatedVar = letter + '=' + final.toString();
+    var completed = isolatedVar;
+  } //2nd if end
+
   } //for end
- otherSide = eval(otherSide);
- console.log('this is otherSide ' + otherSide);
- let eq = new Equation(expression, otherSide);
- let answer = eq.solveFor(letter);
- let answerMap = {};
- answer = answer.toString();
- answerMap['value'] = answer.toString();
- answerMap['letter'] = letter;
- console.log(answerMap);
- return answerMap;
+
+  aAnswer = completed.split('=');
+  if(aAnswer[0].includes(letter)){
+    theAnswer = aAnswer[1];
+  } else {
+    theAnswer = aAnswer[0];
+  }
+  let answerMap = {};
+  answerMap['value'] = theAnswer;
+  answerMap['letter'] = letter;
+  return answerMap;
+  ////////////////////////////////
+  ////////////////////////////////
+
 
 } //solve end
 
 function solveNext(equationsToSolve){
     let lowerCaseRegex = /[a-z]/;
   for(let equation of equationsToSolve){
+    let letterCount = 0;
     for(let i=0; i < equation.length; i++){
-      let letterCount = 0;
       if(lowerCaseRegex.test(equation[i])===true){
         letterCount++;
       }
-
-    if(letterCount === 1){
-      return equation;
-      break;
-     }
    } //for end
+   if(letterCount === 1){
+     return equation;
+     break;
+    }
   } //foreach end
 }
 
@@ -421,6 +421,7 @@ function setCoefficentObjectValue(coefficentObjects, letter, value){
 
 
 function findFirstLetter(theMap){
+
   let lowerCaseRegex = /[a-z]/;
   let found = false;
   let letters = [];
